9344c2e0abaf31a4610da0a6c8a8bb9497c9f97d
bo1atito <shorungbebolatito@gmail.com>
2026-03-14 01:08:06 +0100

    Creating a logo. Removed large file

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,11 +1,13 @@
-import { FcSearch } from "react-icons/fc";
+import { FcSearch } from 'react-icons/fc';
 
 const Logo = () => {
   return (
     <>
-      <div className="border">
-        <h1 className="text-2xl font-bold text-bright">AI</h1>
-        <FcSearch className="size-7 text-black" />
+      <div className="border flex items-center justify-center relative">
+        <h1 className="text-xl font-bold text-bright absolute z-20 top-0 bottom-0">
+          AI
+        </h1>
+        <FcSearch className="size-15 text-black relative z-0" />
       </div>
     </>
   );
